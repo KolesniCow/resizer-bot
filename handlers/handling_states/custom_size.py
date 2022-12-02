84564182fe3ea3914ac299d551fe117d02488cb3
KolesniCow <billybool@billy-debian.billy>
2022-12-02 18:14:15 +0300

    Now bot became more safety

--- handlers/handling_states/custom_size.py
+++ handlers/handling_states/custom_size.py
@@ -8,25 +8,39 @@ from state import ResizeStates
     
 @dp.message_handler(state=ResizeStates.get_width_state)
 async def get_width(message: types.Message, state: FSMContext):
-    answer = message.text
-    await state.update_data(width=answer)
-    await message.answer('Send 📏 height you`re new image:')
-    await ResizeStates.get_height_state.set()
-    
+    try:
+        answer = int(message.text)
+        if answer < 4000:
+            await state.update_data(width=answer)
+            await message.answer('Send 📏 height you`re new image:')
+            await ResizeStates.get_height_state.set()
+        else:
+            await message.answer('It so large!!! Try again: ')
+            await ResizeStates.get_width_state.set()
+    except TypeError:
+        await message.answer('It seems to me what it no width in pixel. Try again: ')
+        await ResizeStates.get_width_state.set()
     
 @dp.message_handler(state=ResizeStates.get_height_state)
 async def get_height(message: types.Message, state: FSMContext):
-    answer = message.text
-    await state.update_data(height=answer)
-    await message.answer('Success... Now, send me you`re image')
-    await ResizeStates.get_image_state.set()
-    
+    try:
+        answer = int(message.text)
+        if answer < 4000:
+            await state.update_data(height=answer)
+            await message.answer('Success... Now, send me you`re image')
+            await ResizeStates.get_image_state.set()
+        else:
+            await message.answer('It so large!!! Try again: ')
+            await ResizeStates.get_height_state.set()
+    except TypeError:
+        await message.answer('It seems to me what it no height in pixel. Try again: ')
+        await ResizeStates.get_height_state.set()
     
 @dp.message_handler(state=ResizeStates.get_image_state, content_types="photo")
 async def push_resized_image(message: types.Message, state: FSMContext):
     height = await state.get_data('height')
     width = await state.get_data('width')
     await bot.send_message(message.from_user.id, 'Resizing process...')
-    await resize_photo(message, w=int(width['width']), h=int(height['height']))
+    await resize_photo(message, w=width['width'], h=height['height'])
     await bot.send_message(message.from_user.id, 'Take you`re changed image.')
     await state.finish()
